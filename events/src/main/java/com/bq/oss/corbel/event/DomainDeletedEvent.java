9ad143f6f9cdf54d38ee8ffbbd2cd711e0354ff4
Luis Miguel Garcia <luismiguel.garcia@bq.com>
2015-05-08 15:26:09 +0200

    Add default constructor to DomainDeletedEvent.
    Change-Id: Ic27bfab0505333768668113a1076f988b127b775

--- events/src/main/java/com/bq/oss/corbel/event/DomainDeletedEvent.java
+++ events/src/main/java/com/bq/oss/corbel/event/DomainDeletedEvent.java
@@ -3,7 +3,11 @@ package com.bq.oss.corbel.event;
 import com.bq.oss.corbel.eventbus.EventWithSpecificDomain;
 
 public class DomainDeletedEvent extends EventWithSpecificDomain {
+
+    public DomainDeletedEvent() {}
+
     public DomainDeletedEvent(String domain) {
         super(domain);
     }
+
 }
