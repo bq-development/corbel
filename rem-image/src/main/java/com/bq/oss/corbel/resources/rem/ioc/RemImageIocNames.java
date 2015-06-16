b52e00854bf4da05a160b6b055e127ecda791efc
Luis Miguel Garcia <luismiguel.garcia@bq.com>
2015-06-16 16:39:12 +0200

    Add rem-image caches invalidation.
    Change-Id: I89b7aee27859eeda8e8e45aff20acbfa99ab2d98

--- rem-image/src/main/java/com/bq/oss/corbel/resources/rem/ioc/RemImageIocNames.java
+++ rem-image/src/main/java/com/bq/oss/corbel/resources/rem/ioc/RemImageIocNames.java
@@ -3,4 +3,7 @@ package com.bq.oss.corbel.resources.rem.ioc;
 public interface RemImageIocNames {
 
     String REM_GET = "ImageGet";
+    String REM_PUT = "ImagePut";
+    String REM_DELETE = "ImageDelete";
+
 }
