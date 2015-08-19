68dae7c5daf0e9f34e43cd6f9ed6e0359e8875d8
Alberto J. Rubio <yebenes9@gmail.com>
2015-08-19 17:48:29 +0200

    Added domain to RemResource path.
    Change-Id: I32ad6d0b48bccaecd3f693bf303d1804de8a810a

--- resources/src/main/java/io/corbel/resources/api/RemResource.java
+++ resources/src/main/java/io/corbel/resources/api/RemResource.java
@@ -42,7 +42,7 @@ import io.corbel.lib.ws.auth.AuthorizationInfo;
  * @author Alexander De Leon
  * 
  */
-@Resource @Path(ApiVersion.CURRENT + "/resource") public class RemResource {
+@Resource @Path(ApiVersion.CURRENT + "/{domain}/resource") public class RemResource {
     private static final Logger LOG = LoggerFactory.getLogger(RemResource.class);
     private final ResourcesService resourcesService;
 
@@ -58,9 +58,9 @@ import io.corbel.lib.ws.auth.AuthorizationInfo;
 
     @GET
     @Path("/{type}")
-    public Response getCollection(@PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
+    public Response getCollection(@PathParam("domain") String domain, @PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
             @Auth AuthorizationInfo authorizationInfo, @Rest QueryParameters queryParameters) {
-        URI typeUri = getBaseUriWithType(uriInfo, type);
+        URI typeUri = getBaseUriWithType(uriInfo, domain, type);
         updateRequestWithLinksTypeAndUri(request, typeUri, type);
         return resourcesService.collectionOperation(type, request, uriInfo, getTokenInfo(authorizationInfo), typeUri, HttpMethod.GET,
                 queryParameters, null, null);
@@ -68,25 +68,25 @@ import io.corbel.lib.ws.auth.AuthorizationInfo;
 
     @POST
     @Path("/{type}")
-    public Response postCollection(@PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
+    public Response postCollection(@PathParam("domain") String domain, @PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
             @Auth AuthorizationInfo authorizationInfo, InputStream inputStream, @HeaderParam("Content-Type") MediaType contentType) {
         return resourcesService.collectionOperation(type, request, uriInfo, getTokenInfo(authorizationInfo),
-                getBaseUriWithType(uriInfo, type), HttpMethod.POST, null, inputStream, contentType);
+                getBaseUriWithType(uriInfo, domain, type), HttpMethod.POST, null, inputStream, contentType);
     }
 
     @DELETE
     @Path("/{type}")
-    public Response deleteCollection(@PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
+    public Response deleteCollection(@PathParam("domain") String domain, @PathParam("type") String type, @Context Request request, @Context UriInfo uriInfo,
             @Auth AuthorizationInfo authorizationInfo, @Rest QueryParameters queryParameters) {
         return resourcesService.collectionOperation(type, request, uriInfo, getTokenInfo(authorizationInfo),
-                getBaseUriWithType(uriInfo, type), HttpMethod.DELETE, queryParameters, null, null);
+                getBaseUriWithType(uriInfo, domain, type), HttpMethod.DELETE, queryParameters, null, null);
     }
 
     @GET
     @Path("/{type}/{id}")
-    public Response getResource(@PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
+    public Response getResource(@PathParam("domain") String domain, @PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
             @Context UriInfo uriInfo, @Auth AuthorizationInfo authorizationInfo, @Rest QueryParameters queryParameters) {
-        URI typeUri = getBaseUriWithType(uriInfo, type);
+        URI typeUri = getBaseUriWithType(uriInfo, domain, type);
         updateRequestWithLinksTypeAndUri(request, typeUri, type);
         return resourcesService.resourceOperation(type, id, request, queryParameters, uriInfo, getTokenInfo(authorizationInfo), typeUri,
                 HttpMethod.GET, null, null, null);
@@ -94,28 +94,28 @@ import io.corbel.lib.ws.auth.AuthorizationInfo;
 
     @PUT
     @Path("/{type}/{id}")
-    public Response putResource(@PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
+    public Response putResource(@PathParam("domain") String domain, @PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
             @Context UriInfo uriInfo, @Auth AuthorizationInfo authorizationInfo, InputStream inputStream,
             @HeaderParam("Content-Type") MediaType contentType, @HeaderParam("Content-Length") Long contentLength,
             @Rest QueryParameters queryParameters) {
         return resourcesService.resourceOperation(type, id, request, queryParameters, uriInfo, getTokenInfo(authorizationInfo),
-                getBaseUriWithType(uriInfo, type), HttpMethod.PUT, inputStream, contentType, contentLength);
+                getBaseUriWithType(uriInfo, domain, type), HttpMethod.PUT, inputStream, contentType, contentLength);
     }
 
     @DELETE
     @Path("/{type}/{id}")
-    public Response deleteResource(@PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
+    public Response deleteResource(@PathParam("domain") String domain, @PathParam("type") String type, @PathParam("id") ResourceId id, @Context Request request,
             @Context UriInfo uriInfo, @Auth AuthorizationInfo authorizationInfo, @Rest QueryParameters queryParameters) {
         return resourcesService.resourceOperation(type, id, request, queryParameters, uriInfo, getTokenInfo(authorizationInfo),
-                getBaseUriWithType(uriInfo, type), HttpMethod.DELETE, null, null, null);
+                getBaseUriWithType(uriInfo, domain, type), HttpMethod.DELETE, null, null, null);
     }
 
     @GET
     @Path("/{type}/{id}/{rel}")
-    public Response getRelation(@PathParam("type") String type, @PathParam("id") ResourceId id, @PathParam("rel") String rel,
+    public Response getRelation(@PathParam("domain") String domain, @PathParam("type") String type, @PathParam("id") ResourceId id, @PathParam("rel") String rel,
             @Context Request request, @Context UriInfo uriInfo, @Auth AuthorizationInfo authorizationInfo,
             @Rest QueryParameters queryParameters, @MatrixParam("r") String resource) {
-        URI typeUri = getBaseUri(uriInfo);
+        URI typeUri = getBaseUri(uriInfo, domain);
         updateRequestWithLinksTypeAndUri(request, typeUri, type);
         return resourcesService.relationOperation(type, id, rel, request, uriInfo, getTokenInfo(authorizationInfo), HttpMethod.GET,
                 queryParameters, resource, null, null);
@@ -157,12 +157,12 @@ import io.corbel.lib.ws.auth.AuthorizationInfo;
         }
     }
 
-    private URI getBaseUri(UriInfo uriInfo) {
-        return uriInfo.getBaseUriBuilder().path(this.getClass()).build();
+    private URI getBaseUri(UriInfo uriInfo, String domain) {
+        return uriInfo.getBaseUriBuilder().path(this.getClass()).build(domain);
     }
 
-    private URI getBaseUriWithType(UriInfo uriInfo, String type) {
-        return uriInfo.getBaseUriBuilder().path(this.getClass()).path(type).build();
+    private URI getBaseUriWithType(UriInfo uriInfo, String domain, String type) {
+        return uriInfo.getBaseUriBuilder().path(this.getClass()).path(type).build(domain);
     }
 
     private TokenInfo getTokenInfo(AuthorizationInfo authorizationInfo) {
