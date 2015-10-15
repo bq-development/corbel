63e57cb149d6f928687003c060185f56730f28fd
Rubén Carrasco <rcarrasco@bq.com>
2015-10-15 11:45:46 +0200

    added hashcode and equals for testing purposes
    Change-Id: Id4e5296c12169cc47b1c3028dea8d54db5e3df37

--- rem-api/src/main/java/io/corbel/resources/rem/request/RequestParametersImpl.java
+++ rem-api/src/main/java/io/corbel/resources/rem/request/RequestParametersImpl.java
@@ -1,12 +1,12 @@
 package io.corbel.resources.rem.request;
 
+import io.corbel.lib.token.TokenInfo;
+
 import java.util.List;
 
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.springframework.http.MediaType;
-
-import io.corbel.lib.token.TokenInfo;
 
 /**
  * @author Alexander De Leon
@@ -70,6 +70,77 @@ public class RequestParametersImpl<E> implements RequestParameters<E> {
     public Long getContentLength() {
         return contentLength;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((acceptedMediaTypes == null) ? 0 : acceptedMediaTypes.hashCode());
+        result = prime * result + ((apiParameters == null) ? 0 : apiParameters.hashCode());
+        result = prime * result + ((contentLength == null) ? 0 : contentLength.hashCode());
+        result = prime * result + ((headers == null) ? 0 : headers.hashCode());
+        result = prime * result + ((params == null) ? 0 : params.hashCode());
+        result = prime * result + ((tokenInfo == null) ? 0 : tokenInfo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        RequestParametersImpl other = (RequestParametersImpl) obj;
+        if (acceptedMediaTypes == null) {
+            if (other.acceptedMediaTypes != null) {
+                return false;
+            }
+        } else if (!acceptedMediaTypes.equals(other.acceptedMediaTypes)) {
+            return false;
+        }
+        if (apiParameters == null) {
+            if (other.apiParameters != null) {
+                return false;
+            }
+        } else if (!apiParameters.equals(other.apiParameters)) {
+            return false;
+        }
+        if (contentLength == null) {
+            if (other.contentLength != null) {
+                return false;
+            }
+        } else if (!contentLength.equals(other.contentLength)) {
+            return false;
+        }
+        if (headers == null) {
+            if (other.headers != null) {
+                return false;
+            }
+        } else if (!headers.equals(other.headers)) {
+            return false;
+        }
+        if (params == null) {
+            if (other.params != null) {
+                return false;
+            }
+        } else if (!params.equals(other.params)) {
+            return false;
+        }
+        if (tokenInfo == null) {
+            if (other.tokenInfo != null) {
+                return false;
+            }
+        } else if (!tokenInfo.equals(other.tokenInfo)) {
+            return false;
+        }
+        return true;
+    }
+
 
 
 }
