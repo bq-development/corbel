d30579b519f69a92d18ebf5f50ff6f69b09c1064
vipereybq <victor.perezrey@bq.com>
2015-10-22 11:53:28 +0200

    [Path 4] SILKROAD-2020 - EventBus should not consume events when launch service cli
    Change-Id: I44b1911fb634a439effeaea1b29dcd9ede50abd3

--- event-bus/src/main/java/io/corbel/eventbus/ioc/EventBusListeningIoc.java
+++ event-bus/src/main/java/io/corbel/eventbus/ioc/EventBusListeningIoc.java
@@ -1,11 +1,13 @@
 package io.corbel.eventbus.ioc;
 
-import java.text.MessageFormat;
-import java.util.Optional;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-
+import io.corbel.eventbus.EventHandler;
+import io.corbel.eventbus.rabbit.EventBusRabbitMQ;
+import io.corbel.eventbus.service.DefaultIncomingEventProcessor;
+import io.corbel.eventbus.service.EventBusRegistry;
+import io.corbel.eventbus.service.InMemoryEventBusRegistry;
+import io.corbel.eventbus.service.IncomingEventProcessor;
+import io.corbel.lib.rabbitmq.config.AmqpConfiguration;
+import io.corbel.lib.rabbitmq.config.BackoffOptions;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -15,20 +17,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
-import io.corbel.eventbus.EventHandler;
-import io.corbel.eventbus.rabbit.EventBusRabbitMQ;
-import io.corbel.eventbus.service.DefaultIncomingEventProcessor;
-import io.corbel.eventbus.service.EventBusRegistry;
-import io.corbel.eventbus.service.InMemoryEventBusRegistry;
-import io.corbel.eventbus.service.IncomingEventProcessor;
-import io.corbel.lib.rabbitmq.config.AmqpConfiguration;
-import io.corbel.lib.rabbitmq.config.BackoffOptions;
+import java.text.MessageFormat;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This IoC loads a context which allow registering {@link EventHandler} in the event bus an start listening for events.
- * 
+ *
  * @author Rubén Carrasco
- * 
+ *
  */
 @Configuration @Import({EventBusIoc.class}) public class EventBusListeningIoc {
 
@@ -62,17 +61,18 @@ import io.corbel.lib.rabbitmq.config.BackoffOptions;
     public AmqpConfiguration eventBusListenerRabbitMQConfiguration(MessageListenerAdapter messageListenerAdapter) {
         ExecutorService threadPoolExecutor = threadPoolExecutor();
         return configurer -> {
-            String queueName = MessageFormat.format(EventBusRabbitMQ.EVENTNBUS_LISTENER_QUEUE_TEMPLATE,
-                    env.getProperty("eventbus.listener.name"));
-            configurer.bind(EventBusRabbitMQ.EVENTBUS_EXCHANGE, configurer.queue(queueName), Optional.empty(), Optional.empty());
-            SimpleMessageListenerContainer container = configurer.listenerContainer(
-                    threadPoolExecutor,
-                    configurer.setRetryOpertations(Optional.ofNullable(env.getProperty("eventbus.maxAttempts", Integer.class)),
-                            Optional.ofNullable(getBackoffOptions())), queueName);
-            container.setQueueNames(queueName);
-            container.setMessageListener(messageListenerAdapter);
-            container.start();
-
+            if(!isConsoleMode()){
+                String queueName = MessageFormat.format(EventBusRabbitMQ.EVENTNBUS_LISTENER_QUEUE_TEMPLATE,
+                        env.getProperty("eventbus.listener.name"));
+                configurer.bind(EventBusRabbitMQ.EVENTBUS_EXCHANGE, configurer.queue(queueName), Optional.empty(), Optional.empty());
+                SimpleMessageListenerContainer container = configurer.listenerContainer(
+                        threadPoolExecutor,
+                        configurer.setRetryOpertations(Optional.ofNullable(env.getProperty("eventbus.maxAttempts", Integer.class)),
+                                Optional.ofNullable(getBackoffOptions())), queueName);
+                container.setQueueNames(queueName);
+                container.setMessageListener(messageListenerAdapter);
+                container.start();
+            }
         };
     }
 
@@ -81,6 +81,10 @@ import io.corbel.lib.rabbitmq.config.BackoffOptions;
         MessageListenerAdapter messageListenerAdapter = new MessageListenerAdapter(incomingEventProcessor(), "process");
         messageListenerAdapter.setMessageConverter(messageConverter);
         return messageListenerAdapter;
+    }
+
+    private boolean isConsoleMode() {
+        return Optional.ofNullable(System.getProperty("mode")).map(mode -> mode!=null && mode.equals("console")).orElse(false);
     }
 
     private BackoffOptions getBackoffOptions() {
