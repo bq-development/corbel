f1aefed8ab27234f12f3d66519cadad079503862
Rubén Carrasco <rcarrasco@bq.com>
2015-10-27 17:55:19 +0100

    added hashcode and equals for testing purposes
    Change-Id: I6e28b2ffe678385aea124341699a8d4401369b44

--- rem-api/src/main/java/io/corbel/resources/rem/request/RelationParametersImpl.java
+++ rem-api/src/main/java/io/corbel/resources/rem/request/RelationParametersImpl.java
@@ -1,14 +1,14 @@
 package io.corbel.resources.rem.request;
 
-import java.util.List;
-import java.util.Optional;
-
 import io.corbel.lib.queries.jaxrs.QueryParameters;
 import io.corbel.lib.queries.request.Aggregation;
 import io.corbel.lib.queries.request.Pagination;
 import io.corbel.lib.queries.request.ResourceQuery;
 import io.corbel.lib.queries.request.Search;
 import io.corbel.lib.queries.request.Sort;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Alexander De Leon
@@ -35,4 +35,33 @@ public class RelationParametersImpl extends CollectionParametersImpl implements 
         return predicate;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((predicate == null) ? 0 : predicate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        RelationParametersImpl other = (RelationParametersImpl) obj;
+        if (predicate == null) {
+            if (other.predicate != null) {
+                return false;
+            }
+        } else if (!predicate.equals(other.predicate)) {
+            return false;
+        }
+        return true;
+    }
 }
