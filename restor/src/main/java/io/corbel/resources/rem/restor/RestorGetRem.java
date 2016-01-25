5957e967e89ae9dc7148b559dcc2d747201dd432
Ismael Madirolas <ismael.madirolas@bq.com>
2016-01-25 16:51:51 +0100

    restor response with content-length
    Change-Id: I0068d583a1e5947de0f8feaa1d24a4ac5ddad410

--- restor/src/main/java/io/corbel/resources/rem/restor/RestorGetRem.java
+++ restor/src/main/java/io/corbel/resources/rem/restor/RestorGetRem.java
@@ -1,37 +1,40 @@
 package io.corbel.resources.rem.restor;
 
+import io.corbel.lib.ws.api.error.ErrorResponseFactory;
+import io.corbel.resources.rem.dao.RestorDao;
+import io.corbel.resources.rem.model.RestorObject;
+import io.corbel.resources.rem.model.RestorResourceUri;
+import io.corbel.resources.rem.request.RequestParameters;
+import io.corbel.resources.rem.request.ResourceId;
+import io.corbel.resources.rem.request.ResourceParameters;
+
 import java.io.InputStream;
 import java.util.Optional;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-
-import io.corbel.lib.ws.api.error.ErrorResponseFactory;
-import io.corbel.resources.rem.model.RestorResourceUri;
-import io.corbel.resources.rem.request.RequestParameters;
-import io.corbel.resources.rem.dao.RestorDao;
-import io.corbel.resources.rem.request.ResourceId;
-import io.corbel.resources.rem.request.ResourceParameters;
-import io.corbel.resources.rem.model.RestorObject;
 
 /**
  * @author Rubén Carrasco
  */
 public class RestorGetRem extends AbstractRestorRem {
 
-	public RestorGetRem(RestorDao dao) {
-		super(dao);
-	}
+    public RestorGetRem(RestorDao dao) {
+        super(dao);
+    }
 
-	@Override
-	public Response resource(String collection, ResourceId resource, RequestParameters<ResourceParameters> parameters,
-			Optional<InputStream> entity) {
+    @Override
+    public Response resource(String collection, ResourceId resource, RequestParameters<ResourceParameters> parameters,
+            Optional<InputStream> entity) {
 
-        RestorResourceUri resourceUri = new RestorResourceUri(parameters.getRequestedDomain(), getMediaType(parameters), collection, resource.getId());
+        RestorResourceUri resourceUri = new RestorResourceUri(parameters.getRequestedDomain(), getMediaType(parameters), collection,
+                resource.getId());
 
         RestorObject object = dao.getObject(resourceUri);
-		if (object != null) {
-			return Response.ok().type(object.getMediaType().toString()).entity(object.getInputStream()).build();
-		}
-		return ErrorResponseFactory.getInstance().notFound();
-	}
+        if (object != null) {
+            return Response.ok().type(object.getMediaType().toString()).entity(object.getInputStream())
+                    .header(HttpHeaders.CONTENT_LENGTH, object.getContentLength()).build();
+        }
+        return ErrorResponseFactory.getInstance().notFound();
+    }
 }
