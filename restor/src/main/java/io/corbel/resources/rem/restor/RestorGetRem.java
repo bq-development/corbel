19d0e5f42148dc031bd7e9e775cef05ea7b7bfb0
Ismael Madirolas <ismael.madirolas@bq.com>
2016-02-12 11:05:24 +0100

    [SILKROAD-2394] restor returns s3 etag
    Change-Id: I161b75fd1fe316e083cd82233cf5551e88a526a2

--- restor/src/main/java/io/corbel/resources/rem/restor/RestorGetRem.java
+++ restor/src/main/java/io/corbel/resources/rem/restor/RestorGetRem.java
@@ -33,7 +33,7 @@ public class RestorGetRem extends AbstractRestorRem {
         RestorObject object = dao.getObject(resourceUri);
         if (object != null) {
             return Response.ok().type(object.getMediaType().toString()).entity(object.getInputStream())
-                    .header(HttpHeaders.CONTENT_LENGTH, object.getContentLength()).build();
+                    .header(HttpHeaders.CONTENT_LENGTH, object.getContentLength()).header(HttpHeaders.ETAG, object.getEtag()).build();
         }
         return ErrorResponseFactory.getInstance().notFound();
     }
