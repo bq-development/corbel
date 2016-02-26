c5e8071be6da62889e5d7ab2ba9c3b0b9657270a
vipereybq <victor.perezrey@bq.com>
2016-02-26 10:10:07 +0100

    [Patch 5][SILKROAD-2397] Webfs urls includes domain
    Change-Id: I882d0bd573fece0925bed9afdee7b89bc19c87a0

--- webfs/src/main/java/io/corbel/webfs/api/WebResource.java
+++ webfs/src/main/java/io/corbel/webfs/api/WebResource.java
@@ -4,15 +4,17 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 import com.amazonaws.services.s3.model.S3Object;
+import io.corbel.lib.ws.auth.AuthorizationInfo;
 import io.corbel.webfs.service.AmazonS3Service;
 import io.corbel.lib.ws.api.error.ErrorResponseFactory;
+import io.dropwizard.auth.Auth;
 
 /**
  * @author Rubén Carrasco
  *
  */
 
-@Path(ApiVersion.CURRENT) public class WebResource {
+@Path(ApiVersion.CURRENT + "/{domain}/path") public class WebResource {
 
     private final AmazonS3Service amazonS3Service;
 
@@ -22,8 +24,10 @@ import io.corbel.lib.ws.api.error.ErrorResponseFactory;
 
     @GET
     @Path("/{path: .*}")
-    public Response getResource(@PathParam("path") String path, @HeaderParam("Accept") String accept) {
-        S3Object object = amazonS3Service.getObject(path);
+    public Response getResource(@PathParam("domain") String domain, @PathParam("path") String path,
+                                @HeaderParam("Accept") String accept) {
+
+        S3Object object = amazonS3Service.getObject(domain +"/"+ path);
         if (object != null) {
             if (accept==null || accept.startsWith("*/*")) {
                 accept = object.getObjectMetadata().getContentType();
@@ -38,8 +42,8 @@ import io.corbel.lib.ws.api.error.ErrorResponseFactory;
 
     @DELETE
     @Path("/{path: .*}")
-    public Response deleteFolder(@PathParam("path") String path){
-        amazonS3Service.deleteFolder(path);
+    public Response deleteFolder(@PathParam("domain") String domain, @PathParam("path") String path ){
+        amazonS3Service.deleteFolder(domain +"/"+ path);
         return Response.noContent().build();
     }
 }
