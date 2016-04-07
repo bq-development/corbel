0f2f5dfad813a9477eac90eaeb6ff73cb59d7238
Rubén Romero Córdoba <ruben.romero@bq.com>
2016-04-07 12:42:53 +0200

    [SILKROAD-2551] Assets should return scopes information UPGRADE
    Change-Id: I516f554490d4bd3245a21aeb5cd175cd966f48ba

--- iam/src/test/java/io/corbel/iam/api/TokenResourceTest.java
+++ iam/src/test/java/io/corbel/iam/api/TokenResourceTest.java
@@ -25,6 +25,7 @@ import javax.ws.rs.core.*;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.security.SignatureException;
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -70,9 +71,9 @@ public class TokenResourceTest {
 
     @ClassRule
     public static ResourceTestRule RULE = ResourceTestRule.builder()
-            .addResource(new TokenResource(authorizationServiceMock, upgradeTokenServiceMock, tokenCookieFactoryMock)).addProvider(filter)
+            .addResource(new TokenResource(authorizationServiceMock, upgradeTokenServiceMock, tokenCookieFactoryMock))
+            .addProvider(filter)
             .addProvider(new AuthorizationInfoProvider().getBinder()).build();
-
     public TokenResourceTest() throws Exception {
     }
 
@@ -238,8 +239,10 @@ public class TokenResourceTest {
         Response response = RULE.client().target(UPGRADE_TOKEN_ENDPOINT).queryParam(ASSERTION, TEST_ASSERTION)
                 .queryParam(GRANT_TYPE, GrantType.JWT_BEARER).request().header(AUTHORIZATION, "Bearer " + TEST_TOKEN).get(Response.class);
 
-        assertThat(response.getStatus()).isEqualTo(204);
-        verify(upgradeTokenServiceMock).upgradeToken(TEST_ASSERTION, tokenReader);
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        List<String> scopesToAdd = upgradeTokenServiceMock.getScopesFromTokenToUpgrade(ASSERTION);
+        verify(upgradeTokenServiceMock).upgradeToken(TEST_ASSERTION, tokenReader, scopesToAdd);
     }
 
     @Test
@@ -249,8 +252,10 @@ public class TokenResourceTest {
         formData.add(ASSERTION, TEST_ASSERTION);
         Response response = RULE.client().target(UPGRADE_TOKEN_ENDPOINT).request().header(AUTHORIZATION, "Bearer " + TEST_TOKEN).post(Entity.form(formData), Response.class);
 
-        assertThat(response.getStatus()).isEqualTo(204);
-        verify(upgradeTokenServiceMock).upgradeToken(TEST_ASSERTION, tokenReader);
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        List<String> scopesToAdd = upgradeTokenServiceMock.getScopesFromTokenToUpgrade(ASSERTION);
+        verify(upgradeTokenServiceMock).upgradeToken(TEST_ASSERTION, tokenReader, scopesToAdd);
     }
 
     @Test
